adb8f72b1a0ebfb2abca71d2a502acb4a3f59daa
Clara Birn <s235640@dtu.dk>
2025-11-15 21:36:56 +0100

    .

---
+++ Activity 48 /Program.cs
@@ -0,0 +1,24 @@
+﻿// Program.cs
+using System;
+
+namespace ArithmeticOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second integer");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(a + b); // sum
+            Console.WriteLine(a * b); // product
+            Console.WriteLine(a - b); // difference
+            Console.WriteLine(a / b); // integer quotient
+            Console.WriteLine(a % b); // remainder
+        }
+    }
+}
